191e1250b91b91e4534e0bbc54fcb9e359678173
DanielEgidio <danielegidio@si.fiponline.edu.br>
2022-04-26 15:55:00 -0300

    Header Styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,15 @@ import React from "react";
 const Header = ({ score }) => {
   return (
     <div className="header">
-      <h1>Score:{score}</h1>
+      <div className="text">
+        <span>Pedra</span>
+        <span>Papel</span>
+        <span>Tesoura</span>
+      </div>
+      <div className="score-box">
+        <span>Score</span>
+        <div className="score-box__score">{score}</div>
+      </div>
     </div>
   );
 };
