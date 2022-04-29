925a532fad0b9ae9fec181da23eff655a0de36fb
DanielEgidio <danielegidio@si.fiponline.edu.br>
2022-04-29 17:46:07 -0300

    footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,41 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Modal from "./Modal";
 const Footer = () => {
-  return <div>Footer</div>;
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => {
+    setModal(!modal);
+  };
+
+  return (
+    <>
+      <footer className="footer">
+        <div class="attribution">
+          Challenge by{" "}
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by{" "}
+          <a
+            href="https://daniel-egidio.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Daniel Egidio
+          </a>
+          .
+        </div>
+        <button className="rules" onClick={toggle}>
+          Rules
+        </button>
+      </footer>
+      {modal ? <Modal toggle={toggle} /> : null}
+    </>
+  );
 };
 
 export default Footer;
